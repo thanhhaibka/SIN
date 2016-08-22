ba78e190e49e134abc015a3445127497e2ff37e0
haithanh <haithanh@gmail.com>
2016-08-22 18:16:05 +0700

    22/8

---
+++ src/main/java/graph/model/Graph.java
@@ -0,0 +1,24 @@
+package graph.model;
+
+import java.util.List;
+
+/**
+ * Created by pc on 18/08/2016.
+ */
+public class Graph {
+    private final List<Vertex> vertexes;
+    private final List<Edge> edges;
+
+    public Graph(List<Vertex> vertexes, List<Edge> edges) {
+        this.vertexes = vertexes;
+        this.edges = edges;
+    }
+
+    public List<Vertex> getVertexes() {
+        return vertexes;
+    }
+
+    public List<Edge> getEdges() {
+        return edges;
+    }
+}
---
+++ src/main/java/graph/model/Vertex.java
@@ -0,0 +1,56 @@
+package graph.model;
+
+import Data.Point;
+
+/**
+ * Created by pc on 18/08/2016.
+ */
+public class Vertex extends Point{
+    final private String id;
+    final private String name;
+
+
+    public Vertex(String id, String name, double x, double y) {
+        super(x, y);
+        this.id = id;
+        this.name = name;
+    }
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Vertex other = (Vertex) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+}
